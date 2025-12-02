bba16c23ee83de960c196c4546521a5f523d6848
DiegoMZurita <thedaired@gmail.com>
2025-12-02 12:49:36 -0300

    feat: Agregar clases Category y Product en model

---
+++ src/main/java/com/dmz/inventory/model/Product.java
@@ -0,0 +1,17 @@
+package com.dmz.inventory.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Integer stock;
+    private Double price;
+    private Long categoryId;
+}
